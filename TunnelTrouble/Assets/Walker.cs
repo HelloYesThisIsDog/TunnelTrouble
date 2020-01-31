20a694a900f4431a6050c2680c6b0b0224242635
HelloYesThisIsDog <johannes_e@web.de>
2020-01-31 21:47:20 +0100

    Walker: DummyAI

--- TunnelTrouble/Assets/Walker.cs
+++ TunnelTrouble/Assets/Walker.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Walker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float    DirectionChangeInterval    = 1.0f;
+    public float    DirectionChangeDelta    = 1.0f;
+    public float    Acceleration            = 1.0f;          
+
+    Rigidbody       m_Rigidbody;
+    float           m_CurAngle              = 0.0f;
+    float           m_NextDirectionChange   = 0;
+
+    private void Awake()
     {
-        
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        m_NextDirectionChange -= Time.deltaTime;
+
+        if (m_NextDirectionChange <= 0)
+        {
+            m_CurAngle += Random.Range(-DirectionChangeDelta, DirectionChangeDelta);
+            m_CurAngle = NormalizeAngle(m_CurAngle);
+            m_NextDirectionChange = DirectionChangeInterval;
+        }
+
+        Vector3 directionVector = Quaternion.AngleAxis(m_CurAngle, Vector3.up) * Vector3.right;
+
+        m_Rigidbody.AddForce(Acceleration * directionVector , ForceMode.VelocityChange);
     }
+
+	public static float NormalizeAngle(float angle)
+	{
+		float result = angle - Mathf.CeilToInt(angle / 360f) * 360f;
+		if (result < 0)
+		{
+			result += 360f;
+		}
+		return result;
+	}
 }
