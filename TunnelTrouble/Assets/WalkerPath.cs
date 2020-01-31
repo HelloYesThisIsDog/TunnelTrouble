460e482e228436571c4ae4b273bd58bf2c325e25
HelloYesThisIsDog <johannes_e@web.de>
2020-01-31 22:10:50 +0100

    WalkerPath: v1

---
+++ TunnelTrouble/Assets/WalkerPath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkerPath : MonoBehaviour
+{
+    public static WalkerPath s_WalkerPath;
+    
+    private Transform[] m_PathPoints;
+
+    void Awake()
+    {
+        s_WalkerPath = this;
+
+        m_PathPoints = new Transform[transform.childCount];
+
+        for (int c = 0; c < transform.childCount; ++c)
+        {
+            m_PathPoints[c] = transform.GetChild(c);
+        }
+    }
+
+}
