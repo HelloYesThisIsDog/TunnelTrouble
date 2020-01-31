cb6d14dc17b2a02b749136e8a37c186e59c18897
HelloYesThisIsDog <johannes_e@web.de>
2020-01-31 22:59:54 +0100

    Walker: pathfollowing++

---
+++ TunnelTrouble/Assets/Scripts/Editor/WalkerSpawnerEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WalkerSpawner))]
+public class WalkerSpawnerEditor : Editor
+{
+	///////////////////////////////////////////////////////////////////////////
+
+	public override void OnInspectorGUI()
+	{
+		WalkerSpawner walkerSpawner = (WalkerSpawner)target;
+
+		DrawDefaultInspector();
+
+		// We only want to modify Scene data during play mode
+		GUI.enabled = Application.isPlaying;
+
+		if (GUILayout.Button("KillAll"))
+		{
+			walkerSpawner.KillAllWalkers();
+		}
+
+		if (GUILayout.Button("Spawn Single"))
+		{
+			walkerSpawner.SpawnWalker();
+		}
+
+		if (GUILayout.Button(walkerSpawner.enabled ? "Stop Spawning" : "Resume Spawning"))
+		{
+			walkerSpawner.enabled = !walkerSpawner.enabled;
+		}
+
+		GUI.enabled = true;
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+}
--- TunnelTrouble/Assets/Scripts/WalkerSpawner.cs
+++ TunnelTrouble/Assets/Scripts/WalkerSpawner.cs
@@ -7,24 +7,52 @@ public class WalkerSpawner : MonoBehaviour
     public Walker   WalkerSpawnPrefab;
     public float    SpawnBurstInterval;
     public int      SpawnsPerBurst;
+    public int      MaxWalkerCount = 20;
+    public int      MaxSpawnCount  = int.MaxValue;
 
     private float   m_LastSpawnTime = float.NegativeInfinity;
+    private int     m_SpawnedCount = 0;
+
+    bool NeedsMoreWalkers()
+    {
+		bool enoughWalkersAlive = transform.childCount >= MaxWalkerCount;
+		bool enoughWalkersSpawned = m_SpawnedCount >= MaxSpawnCount;
+
+        return !enoughWalkersAlive && !enoughWalkersSpawned;
+	}
 
     private void Update()
     {
-        if (Time.time - m_LastSpawnTime > SpawnBurstInterval)
+        bool intervalFinished = Time.time - m_LastSpawnTime > SpawnBurstInterval;
+
+        if (intervalFinished)
         {
             m_LastSpawnTime = Time.time;
 
             for (int i = 0; i < SpawnsPerBurst; ++i)
             {
+                if (!NeedsMoreWalkers())
+                {
+                    break;
+                }
                 SpawnWalker();
             }
         }
     }
 
+    public void KillAllWalkers()
+    {
+		for (int c = 0; c < transform.childCount; ++c)
+		{
+			GameObject.Destroy(transform.GetChild(c).gameObject);
+        }
+    }
+
     public void SpawnWalker()
     {
-        GameObject.Instantiate(WalkerSpawnPrefab, transform);
+        Walker walker = GameObject.Instantiate(WalkerSpawnPrefab, transform);
+        walker.gameObject.name = "Walker " + m_SpawnedCount;
+
+        m_SpawnedCount++;
     }
 }
