0217a5e74be4a7d0ebda9ae25297d05350b02ebc
HelloYesThisIsDog <johannes_e@web.de>
2020-01-31 21:22:49 +0100

    CharacterController: Bad Controller v2

--- TunnelTrouble/Assets/PlayerController.cs
+++ TunnelTrouble/Assets/PlayerController.cs
@@ -4,22 +4,55 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 
-	[SerializeField] private float m_Acceleration;
+	public float Speed				= 5f;
+	public float JumpHeight			= 2f;
+	public float GroundDistance		= 0.2f;
+	public float DashDistance		= 5f;
+	public LayerMask GroundLayer;
 
 	private Rigidbody m_Rigidbody;
+	private Vector3 m_Inputs		= Vector3.zero;
+	private bool m_IsGrounded		= true;
 
-	void Awake()
+	void Start()
 	{
-		m_Rigidbody = GetComponent<Rigidbody>();
+		m_Rigidbody		= GetComponent<Rigidbody>();
 	}
+
+	void Update()
+	{
+		m_IsGrounded = Physics.CheckSphere(transform.position, GroundDistance, GroundLayer, QueryTriggerInteraction.Ignore);
+
+		m_Inputs = Vector3.zero;
+		m_Inputs.x = Input.GetAxis("Horizontal");
+		m_Inputs.z = Input.GetAxis("Vertical");
+		if (m_Inputs != Vector3.zero)
+		{
+			transform.forward = m_Inputs;
+		}
+
+		if (Input.GetButtonDown("Jump") && m_IsGrounded)
+		{
+			m_Rigidbody.AddForce(Vector3.up * JumpHeight, ForceMode.VelocityChange);
+		}
+		if (Input.GetButtonDown("Dash"))
+		{
+			Vector3 dashVelocity = Vector3.Scale(transform.forward, DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * m_Rigidbody.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * m_Rigidbody.drag + 1)) / -Time.deltaTime)));
+			m_Rigidbody.AddForce(dashVelocity, ForceMode.VelocityChange);
+		}
+	}
+
 
 	void FixedUpdate()
 	{
-		float moxeH = Input.GetAxis("Horizontal");
-		float moveV = Input.GetAxis("Vertical");
-
-		Vector3 moveDir = new Vector3(moxeH, 0.0f, moveV);
-
-		m_Rigidbody.AddForce(moveDir * m_Acceleration);
+		m_Rigidbody.MovePosition(m_Rigidbody.position + m_Inputs * Speed * Time.fixedDeltaTime);
 	}
+
+
+
+
+
+
+
+
 }
