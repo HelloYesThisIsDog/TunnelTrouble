0763935fe785cd5685c49783a787201c838657fa
patrickwarnatsch <patrick_warnatsch@hotmail.de>
2020-02-01 21:03:38 +0100

    created pipe and puddle prefab

---
+++ TunnelTrouble/Assets/Pipe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+	public ParticleSystem steam;
+	public Trap trap;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+		if (trap.m_State == TrapState.Broken_WaitForFix)
+		{
+			steam.Play();
+		}
+
+		if (trap.m_State == TrapState.WaitingForAttack)
+		{
+			steam.Stop();
+		}
+	}
+}
