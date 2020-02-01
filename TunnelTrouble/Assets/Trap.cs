486d73644f39d85839aba50701da2077238c8825
HelloYesThisIsDog <johannes_e@web.de>
2020-02-01 10:18:47 +0100

    Scene: ++

--- TunnelTrouble/Assets/Trap.cs
+++ TunnelTrouble/Assets/Trap.cs
@@ -12,7 +12,7 @@ public enum TrapState
 
 public class Trap : MonoBehaviour
 {
- /*   public float AttackCooldownMin      = 10.0f;
+    public float AttackCooldownMin      = 10.0f;
     public float AttackCooldownMax      = 15.0f;
     public float AttackWarningDuration  = 10.0f;
 
@@ -32,17 +32,27 @@ public class Trap : MonoBehaviour
 
     private void ChangeToState(TrapState forceSetState)
     {
-        
-    }
+		switch (m_State)
+		{
+			case TrapState.WaitingForAttack:
+				m_NextStateChange = Random.Range(AttackCooldownMin, AttackCooldownMax); break;
+			case TrapState.Warning:
+                m_NextStateChange = AttackWarningDuration;                   			break;
+			case TrapState.Attacking_WaitForFix:                        				break;
+			case TrapState.Broken_WaitForFix:                                   		break;
+			
+                default: Debug.Assert(false); break;
+		}
+	}
 
     private void Update()
     {
-        float timeSinceLastStateChange = Time.time - m_LastStateChange;
+       // float timeSinceLastStateChange = Time.time - m_LastStateChange;
 
         switch ( m_State )
         {
             case TrapState.WaitingForAttack:
-                if (timeSinceLastStateChange > )
+                //if (timeSinceLastStateChange > )
                 break;
             case TrapState.Warning:
                 break;
@@ -53,5 +63,5 @@ public class Trap : MonoBehaviour
             default: Debug.Assert(false);                break;
 
         }
-    }*/
+    }
 }
