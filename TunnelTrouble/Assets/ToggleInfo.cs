3aa2224c315bbdb3b4bf273a5ae64a5ac7413a33
iljush <ilya.den.volkov@gmail.com>
2020-02-02 15:33:33 +0100

    Infographic added

---
+++ TunnelTrouble/Assets/ToggleInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleInfo : MonoBehaviour
+{
+    public Transform Infographic;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            if (Infographic.gameObject.activeSelf)
+            {
+                Infographic.gameObject.SetActive(false);
+            }
+            else
+            {
+                Infographic.gameObject.SetActive(true);
+
+            }
+
+        }
+    }
+}
