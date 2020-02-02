0329ba601024007f9f88146112edcbf48ab6743e
patrickwarnatsch <patrick_warnatsch@hotmail.de>
2020-02-02 12:38:47 +0100

    added player marker

---
+++ TunnelTrouble/Assets/PlayerMarker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMarker : MonoBehaviour
+{
+	public PlayerController pc;
+	public GameObject ring;
+
+	private Material mat;
+	private PlayerSlot slot;
+
+    void Start()
+    {
+		mat = ring.GetComponent<Renderer>().material;
+		slot = pc.Slot;
+		mat.SetColor("_EmissionColor", PlayerController.GetPlayerColor(slot));
+    }
+}
